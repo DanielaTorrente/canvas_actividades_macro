9661d0b8ca9a8a53a2f596569f9a6461ad852596
DanielaTorrente <danielaatorrente@gmail.com>
2025-04-29 12:28:15 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CanvasActividades from "./componentes/CanvasActividades";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <CanvasActividades />
+    </div>
+  );
+}
+
+export default App;
